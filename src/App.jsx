0b47ddf50d6da2577ec7fff13e3252f07f763991
teacherIan <iansmalloy@gmail.com>
2024-06-06 13:56:56 +0800

    visual updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { doc, getDoc, getDocs, collection } from 'firebase/firestore';
 import { db } from './db';
 import './App.css';
+import logo from './assets/logo.jpg';
 
 function App() {
   const [data, setData] = useState([]);
@@ -18,7 +19,7 @@ function App() {
           console.log(doc.id, ' => ', doc.data());
         });
         newData.sort((a, b) => b.total - a.total);
-        setData(newData);
+        setData(newData.slice(0, 3));
       } catch (error) {
         setError(error);
         console.error('Error fetching data:', error);
@@ -29,12 +30,31 @@ function App() {
   }, []);
 
   return (
-    <div>
-      {data.map((item) => (
-        <div key={item.id}>
-          <h2>{item.name}</h2> <p>{item.total}</p>{' '}
-        </div>
-      ))}
+    <div className="container">
+      {data.length > 0 && (
+        <>
+          <div className="gold">
+            <img className="logo" src={logo}></img>
+            <div className="name">FIRST</div>
+            <div className="name">{data[0].name}</div>
+            <div className="total">{data[0].total}</div>
+          </div>
+          <div className="silver">
+            <img className="logo" src={logo}></img>
+            <div className="name">SECOND</div>
+            <div className="name">{data[1].name}</div>
+            <div className="total">{data[1].total}</div>
+          </div>
+          <div className="bronze">
+            <img className="logo" src={logo}></img>
+            <div className="name">THIRD</div>
+            <div className="name">{data[2].name}</div>
+            <div className="total">{data[2].total}</div>
+          </div>
+        </>
+      )}
+      {data.length === 0 && <div>Loading...</div>}
+      {error && <div>Error: {error.message}</div>}
     </div>
   );
 }
